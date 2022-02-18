51daef697666279e5e61d7c0f78d0a152e483633
gabriele.saracino9904 <gabriele.saracino9904@gmail.com>
2022-02-18 11:07:27 +0100

    graphlookup usato

--- Pippo.py
+++ Pippo.py
@@ -1,32 +1,43 @@
 import asyncio
-from asyncio.windows_events import NULL
 from collections import defaultdict
-from typing import List
+from bson.objectid import ObjectId
+import motor
+import motor.motor_asyncio
+from asyncio.windows_events import NULL
+from typing import DefaultDict, List
 import pymongo
-baseCollection=[("A0","0"),("A1","A0"),("A2","A0"),("A3","A0"),("A4","A1"),("A5","A1")]
-client = pymongo.MongoClient("mongodb+srv://gabboUser:gabbopsw@cluster0.4ln68.mongodb.net/dbpippo?retryWrites=true&w=majority")
-db = client["dbpippo"]
-pippoCol=db["Pippo"]
-class Pippo():
-    def __init__(self,idChain,chain):
-        self.idChain=idChain
-        self.chain=chain
+
+client = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://gabboUser:gabbopsw@cluster0.4ln68.mongodb.net/dbpippo?retryWrites=true&w=majority")
+db = client["pippodb"]
+pippoCol=db["pippo"]
+
+async def GetFamily(idPippo):
+    lsp=defaultdict(list)
+    countPippo=db.pippo.count_documents()
+    lsp= db.pippo.aggregate([
+        {
+            "$graphLookup":{
+                'from': 'pippo',
+                'startWith': '$ID',
+                'connectFromField': 'ID',
+                'connectToField': 'IDChain',
+                #'restrictSearchWithMatch': { "ID" : "1" },
+                'as': 'chain'
+            }
+         }
+        ])
+    #lst=lsp.toArray()
+    #lst=lsp.find()
+    #lsp=defaultdict(list)
+    #lst=await lsp.to_list(5)
+    lst=defaultdict(list)
+    async for x in lsp:
+        lst[x.get("ID")].append(x.get("IDChain"))
+    for x in lst.items():
+        print(x)
+        
     
 
-async def GetFamily(pippo):
-    res = defaultdict(list)
-    for child,parent in pippo:
-        res[parent].append(child)
-    #PippoFamily=[]
-    #queryPippos = { "idChain": pippo.id }
-    #ppsns=pippoCol.find(queryPippos)
-    #for x in ppsns:
-    #    PippoFamily.append(Pippo(x.get('_id'),x.get('idChain')))
-    #queryPippos = { "idChain": pippo.idChain }
-    #ppfth=pippoCol.find_one(queryPippos)
-    #PippoFamily.append(Pippo(ppfth.get('_id'),ppfth.get('idChain')))
-    return res
+asyncio.get_event_loop().run_until_complete(GetFamily("620eacb8f4c3bc6d240053b8"))
+#asyncio.run(GetFamily())
 
-#asyncio.run(GetFamily(Pippo('620d427d741cf19625508a42','620d425c741cf19625508a40')))
-a=asyncio.run(GetFamily(baseCollection))
-print(a)
