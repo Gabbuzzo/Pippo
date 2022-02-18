b3d6022b8ba89cac0509f2b8cb11c1c2650714fe
gabriele.saracino9904 <gabriele.saracino9904@gmail.com>
2022-02-18 18:40:26 +0100

    fatto

--- Pippo.py
+++ Pippo.py
@@ -7,6 +7,7 @@ from asyncio.windows_events import NULL
 from typing import DefaultDict, List
 import pymongo
 from enum import Enum
+import bson
 
 client = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://gabboUser:gabbopsw@cluster0.4ln68.mongodb.net/dbpippo?retryWrites=true&w=majority")
 db = client["pippodb"]
@@ -62,9 +63,16 @@ async def GetFamily(idPippo):
                     lst.chain.append(p)
             #if x.get("")
     for x in lst.chain:
-        print(x.type_name)
-    family=parentela(lst.main_object_id,list(lst.chain))
-    result = await parCol.insert_one(family)
+        print(x.type_name.name)
+    pipposDict=defaultdict(list)
+    for i in lst.chain:
+        if i.id_chain!=None:
+            pipposDict[i.id_chain].append(i.type_name.name)
+        else:
+            pipposDict["0"].append(i.type_name.name)
+    family=parentela(lst.main_object_id,pipposDict)
+    a=bson.BSON.encode(family.__dict__)
+    result = await parCol.insert_one(family.__dict__)
     return lst    
 
 asyncio.get_event_loop().run_until_complete(GetFamily("620f56842c357ed1ca5c7830"))
